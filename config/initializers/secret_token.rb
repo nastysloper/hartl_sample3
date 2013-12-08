2a9bbc62baaa0c41898940fc09d50151ab7dab7a
Rich Vogt <rvogt@thebrighttag.com>
2013-12-08 10:04:53 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '74fe7779ec269045ba8e887fca13359912efcd93a9556793a9f3d875915a91af4feb4f05d05f77305c310350684b5d8c656211aeeb496506bc5239ae4ef39a5a'
