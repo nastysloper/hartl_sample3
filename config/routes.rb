436054a570a7a29de6a9927c1e4e6d722c05ca0b
Rich Vogt <richvogtjr@gmail.com>
2013-12-21 12:48:28 -0600

    update routes so tests pass, add welcome controller and move welcome page to views welcome directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # match '/', to: 'static_pages#home'
+  root :to => 'welcome#index'
 
-  get "static_pages/about"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/contact"
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,10 +58,7 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
