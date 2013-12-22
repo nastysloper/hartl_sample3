d01e6b22176bc98322baddf0074372d67dfab1ce
Rich Vogt <richvogtjr@gmail.com>
2013-12-22 08:53:54 -0600

    complete let statements for static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,14 +30,16 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector 'h1', text: 'About Us' }
-    it { should have_selector 'title', text: full_title('About') }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector 'h1', text: 'Contact' }
-    it { should have_selector 'title', text: full_title('Contact') }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
   end
 end
