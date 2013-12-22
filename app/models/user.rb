9a25c8be298431c2513848d88d637daa45e37ebc
Rich Vogt <richvogtjr@gmail.com>
2013-12-22 14:39:25 -0600

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  # database adapters do not use case-insensitive indices. Hartl, page 253.
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,48 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: "Essie Sample User", email: "essie@user.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when name is over 50 characters" do
+    before { @user.name = 'a' * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                      foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
