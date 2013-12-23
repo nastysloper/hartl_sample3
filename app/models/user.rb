e47f15db8bd278bbe9caa15a6bc5f18b142a50ab
Rich Vogt <richvogtjr@gmail.com>
2013-12-22 21:46:41 -0600

    add test for mixed-case email

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   # database adapters do not use case-insensitive indices. Hartl, page 253.
-  before_save { |user| user.email = email.downcase }
+  # before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
