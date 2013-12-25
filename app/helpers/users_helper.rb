a9b7bf4864f95af1df0a6f48507f135d54834d85
Rich Vogt <richvogtjr@gmail.com>
2013-12-25 12:41:03 -0600

    add styling for gravatar and debug gravatar link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,11 @@
 module UsersHelper
+
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}"
+    # the following line was used to debug the url
+    # image_tag('https://s.gravatar.com/avatar/0632cac8e7c2122efdd45f93657737ec?s=80', alt: user.name, class: "gravatar")
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
