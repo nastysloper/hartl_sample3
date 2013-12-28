8e6262e614e47c2d2508d7d19d886a7f700cc683
Rich Vogt <richvogtjr@gmail.com>
2013-12-28 16:07:01 -0600

    fix ssl bug no two

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-# Force all access to the top over SSL, use Strict-Transport-Security
-# and use secure cookies
-#
-# SSL is now actually TSL, Transport Layer Security
-config.force_ssl = true
