21a80c33fdbc805cce0424811264cd9366c5e5df
Rich Vogt <richvogtjr@gmail.com>
2013-12-29 13:00:14 -0600

    add tests for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ SampleApp::Application.routes.draw do
   # match '/', to: 'static_pages#home'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root :to => 'welcome#index'
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help'
 
