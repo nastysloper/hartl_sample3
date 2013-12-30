2f601ea4f443182dd4b2cef0fc559fd3bd01b84a
Rich Vogt <richvogtjr@gmail.com>
2013-12-29 19:24:49 -0600

    add remember token to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:email])
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     else
-      flash.now[:error] = 'Invalid email/password combination' # not yet right
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
