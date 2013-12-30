f58a90904dd7d38c976ff6add057e5eb68ad2ea7
Rich Vogt <richvogtjr@gmail.com>
2013-12-29 18:19:58 -0600

    add flash error message for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:email])
+      # Sign the user in and redirect to the user's show page.
+    else
+      flash.now[:error] = 'Invalid email/password combination' # not yet right
+      render 'new'
+    end
   end
 
   def destroy
