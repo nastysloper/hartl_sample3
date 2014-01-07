f2c5c99662d6346411b9c1e6785de107f14f9c2f
Rich Vogt <richvogtjr@gmail.com>
2014-01-06 19:40:32 -0600

    add sum tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -55,12 +55,12 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email", with: user.email
+          fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
+          click_button "Sign in"
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
@@ -77,6 +77,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the users index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
     end
