f9e478f8edafe63fa92d0a4bc0967cefa856d349
Rich Vogt <richvogtjr@gmail.com>
2014-01-25 15:40:19 -0600

    fixed bug that prevented microposts from being created as well as incorrect routing on the welcome root page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
-    @micropost = current_user.microposts.build(params[:microposts])
+    @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
-  root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
