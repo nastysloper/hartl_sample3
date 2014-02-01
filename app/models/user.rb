ce8d16b06cb8ad2e6b947d53d51a3c5ed7a7ca80
Rich Vogt <richvogtjr@gmail.com>
2014-02-01 13:42:58 -0600

    fix bugs and add code

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   # This one method, along with password digest column in the database
   # allows Rails to securely create and authenticate new users.
@@ -35,6 +40,18 @@ class User < ActiveRecord::Base
   def feed
     # This is preliminary
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
