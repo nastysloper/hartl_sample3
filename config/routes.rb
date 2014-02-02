92940d2a873ab804bba4a061420aea7889cb3d40
Rich Vogt <richvogtjr@gmail.com>
2014-02-02 13:57:01 -0600

    adding all the things

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # match '/', to: 'static_pages#home'
-
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root :to => 'static_pages#home'
 
